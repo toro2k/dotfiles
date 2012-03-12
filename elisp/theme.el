c0928248c644a3fd4f054b05b5cd19ba77d72b2b
toro2k <toro2k@gmail.com>
2012-03-12 22:18:44 +0100

    Terminus as emacs and urxvt font.

--- elisp/theme.el
+++ elisp/theme.el
@@ -13,7 +13,7 @@
   (set-face-attribute 'default nil :foreground white
 		      :background black)
   (if (member "DejaVu Sans Mono" (font-family-list))
-      (set-face-attribute 'default nil :font "DejaVu Sans Mono-10"))
+      (set-face-attribute 'default nil :font "-*-terminus-bold-*-*-*-16-*-*-*-*-*-*-*"))
 
 
   ;; UI faces
